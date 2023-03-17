5298b295c9bfc10ea9db403e42eb22806ebaa69b
hp <uvaneswarvelmurugan@gmail.com>
2023-03-17 12:26:53 +0530

    commit

--- src/test/java/pages/LoginPage.java
+++ src/test/java/pages/LoginPage.java
@@ -2,11 +2,13 @@ package pages;
 
 import java.time.Duration;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
 
 import base.TestBase;
 
@@ -35,6 +37,8 @@ public class LoginPage extends TestBase {
 		signInCnfrm.click();
 		WebDriverWait wait = new WebDriverWait(driver,Duration.ofSeconds(30));
 		wait.until(ExpectedConditions.textToBePresentInElement(username, "Welcome Uvaneswar"));
+		boolean isDisp  = driver.findElement(By.xpath("//a[contains(text(),'Welcome Uvaneswar')]")).isDisplayed();
+		Assert.assertTrue(isDisp);
 //		return new AddSingleItem();
 	}
 //	public void userName() {
